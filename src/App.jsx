86871dbc71579e97b2b53601705dfe30f4f0ae33
Dina <dinagiacobbi@gmail.com>
2025-01-12 12:59:39 -0900

    Adding filtering function for categories

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,35 @@ import { useState } from 'react'
 import menu from './data'
 import Title from './components/Title'
 import Menu from './components/Menu'
+import Categories from './components/Categories'
 
+const allCategories = [
+  'all',
+  ...new Set(menu.map((item) => item.category)),
+]
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filteredItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(menu)
+      return
+    }
+    const newItems = menu.filter((item)=> item.category === category)
+    setMenuItems(newItems)
+  }
 
   return (
     <>
-    
-      <main className='world'>
+      <main className="world">
         <section className="menu">
-          <Title text='our menu' />
+          <Title text="our menu" />
+          <Categories
+            categories={categories}
+            filteredItems={filteredItems}
+          />
           <Menu items={menuItems} />
         </section>
       </main>
---
+++ src/components/Categories.jsx
@@ -0,0 +1,24 @@
+
+const Categories = ({ categories, filteredItems }) => {
+  return (
+    <>
+      <div className="btn-container">
+        {categories.map((category) => {
+          return (
+            <button
+              type="button"
+              className="btn"
+              key={category}
+              onClick={() => filteredItems(category)}
+            >
+             
+              {category}
+            </button>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Categories
