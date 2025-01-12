5625c1e569ed9da35ac42c99da84ddd98b36da42
Dina <dinagiacobbi@gmail.com>
2025-01-12 10:46:08 -0900

    Removed bg image, adding props to MenuItem file

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,16 +1,18 @@
-
-import React from 'react'
-
-const MenuItem = () => {
+const MenuItem = ({ img, title, price, desc }) => {
   return (
     <>
-      <h4>Placeholder for Menu Item</h4>
+      <article className="menu-item">
+        <img src={img} alt={title} className="img" />
+        <div className="item-info">
+          <header>
+            <h5> {title}</h5>
+            <span className="item-price"> ${price}</span>
+          </header>
+          <p className="item-text">{desc}</p>
+        </div>
+      </article>
     </>
   )
 }
 
 export default MenuItem
-
-
-
-
